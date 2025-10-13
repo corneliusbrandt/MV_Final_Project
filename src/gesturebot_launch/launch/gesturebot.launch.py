bd2a15984f099be700fb882aab8387da51123446
fynns <fynns@uia.no>
2025-10-14 00:51:12 +0200

    cleanup

--- src/gesturebot_launch/launch/gesturebot.launch.py
+++ src/gesturebot_launch/launch/gesturebot.launch.py
@@ -1,7 +1,4 @@
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
-from launch.substitutions import PathJoinSubstitution
-from launch_ros.substitutions import FindPackageShare
 
 from launch_ros.actions import Node
 
--- src/gesturebot_launch/launch/gesturebot_sim.launch.py
+++ src/gesturebot_launch/launch/gesturebot_sim.launch.py
@@ -2,8 +2,6 @@ from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.actions import IncludeLaunchDescription
 from launch.substitutions import LaunchConfiguration
-from launch.substitutions import PathJoinSubstitution
-from launch_ros.substitutions import FindPackageShare
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
 import os
